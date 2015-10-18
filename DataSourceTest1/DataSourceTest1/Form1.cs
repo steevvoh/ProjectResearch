cb5608bbc78c7ab2d1afaaef66f44d690b3e072c
Steve Olsen <stephen.james.olsen@gmail.com>
2015-10-18 13:48:27 -0700

    Container Test
    Container Test and Data Sender

---
+++ DataSourceTest1/DataSourceTest1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace DataSourceTest1
+{
+    public partial class Form1 : Form
+    {
+        struct data
+        {
+            public float a;
+            public float b;
+            public float c;
+            public int i;
+        }
+
+        private data d;
+        private UdpClient uc;
+        private Byte[] sendBytes;
+        private ulong packetcount;
+        private Stopwatch _stopwatch;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.uc = new UdpClient();
+            this.uc.Connect("localhost", 11000);
+            this._stopwatch = new Stopwatch();
+            this._stopwatch.Start();
+            
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            packdata();
+            senddata();
+        }
+
+        private void packdata()
+        {
+            this.d.a = singen(3.0f, 0.2f);
+            this.d.b = singen(5.5f, 1.0f);
+            this.d.c = singen(1.1f, 0.4f);
+            this.d.i = (int)singen(16.0f, 0.5f);
+                
+        }
+
+        private float singen(float amplitude, float period)
+        {
+            double angle;
+            angle = this._stopwatch.ElapsedMilliseconds / (period * 1000.0);
+            return (float)Math.Sin(angle) * amplitude;
+        }
+
+        private void senddata()
+        {
+            this.sendBytes = _getBytes(this.d);
+            this.uc.Send(this.sendBytes, this.sendBytes.Length);
+            this.packetcount++;
+            this.label1.Text = this.packetcount.ToString();
+        }
+
+        private byte[] _getBytes(data _data)
+        {
+            int size = Marshal.SizeOf(_data);
+            byte[] arr = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(_data, ptr, true);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+    }
+}
---
+++ containertest3/containertest3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace containertest3
+{
+
+
+    public partial class Form1 : Form
+    { 
+        private UdpClient ucreciever;
+        private IPEndPoint RemoteIpEndPoint;
+        private Byte[] receivebytes;
+        private data d;
+
+        struct data
+        {
+            public float a;
+            public float b;
+            public float c;
+            public int i;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ucreciever = new UdpClient(11000);
+            //this.RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            //this.receivebytes = this.ucreciever.Receive(ref this.RemoteIpEndPoint);
+            //this.label1.Text = Encoding.ASCII.GetString(this.receivebytes);
+            this.ucreciever.BeginReceive(new AsyncCallback(recv), null);
+
+        }
+
+        private void recv(IAsyncResult res)
+        {
+            this.RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            this.receivebytes = this.ucreciever.EndReceive(res, ref RemoteIpEndPoint);
+            this.ucreciever.BeginReceive(new AsyncCallback(recv), null);
+            viewchange();
+        }
+
+        private void viewchange()
+        {
+            SetText( ByteArrayToString(this.receivebytes), this.label1);
+            this.d = fromBytes(this.receivebytes);
+            SetText(this.d.a.ToString(), this.label2);
+            SetText(this.d.b.ToString(), this.label3);
+            SetText(this.d.c.ToString(), this.label4);
+            SetText(this.d.i.ToString(), this.label5);
+        }
+
+        delegate void SetTextCallback(string text, Label label);
+
+        private void SetText(string text, Label label)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (label.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text, label });
+            }
+            else
+            {
+                label.Text = text;
+            }
+        }
+
+
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        data fromBytes(byte[] arr)
+        {
+            data str = new data();
+
+            int size = Marshal.SizeOf(str);
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+
+            Marshal.Copy(arr, 0, ptr, size);
+
+            str = (data)Marshal.PtrToStructure(ptr, str.GetType());
+            Marshal.FreeHGlobal(ptr);
+
+            return str;
+        }
+    }
+}
